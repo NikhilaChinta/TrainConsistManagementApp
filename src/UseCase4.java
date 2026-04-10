1993fb62b846f1315ae76feab5263e33aa56d16a
NikhilaChinta <nikhilachinta21@gmail.com>
2026-04-10 15:12:31 +0530

    UC4: clean branch with only UseCase4

--- src/UseCase4.java
+++ src/UseCase4.java
@@ -1,20 +1,8 @@
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.*;
 
 public class UseCase4 {
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
-
-        SortedSet<String> bogieIds = new TreeSet<>();
-        bogieIds.add("BG105");
-        bogieIds.add("BG101");
-        bogieIds.add("BG103");
-        bogieIds.add("BG102");
-        bogieIds.add("BG104");
-        bogieIds.add("BG101"); // duplicate
-
-        System.out.println("Sorted Unique Bogie IDs: " + bogieIds);
-        System.out.println("First Bogie: " + bogieIds.first());
-        System.out.println("Last Bogie : " + bogieIds.last());
+        System.out.println("Running UseCase4...");
     }
 }
