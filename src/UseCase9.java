7c03285bb3fc5b3a8720f838d582e60d48b7a554
NikhilaChinta <nikhilachinta21@gmail.com>
2026-04-10 15:12:41 +0530

    UC9: clean branch with only UseCase9

--- src/UseCase9.java
+++ src/UseCase9.java
@@ -1,8 +1,8 @@
 import java.util.*;
 
-public class UseCase8 {
+public class UseCase9 {
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
-        System.out.println("Running UseCase8...");
+        System.out.println("Running UseCase9...");
     }
 }
