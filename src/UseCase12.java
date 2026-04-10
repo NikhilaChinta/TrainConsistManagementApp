a0da2484432c70ee6f30d4bb8f034d1464fd9ede
NikhilaChinta <nikhilachinta21@gmail.com>
2026-04-10 15:12:47 +0530

    UC12: clean branch with only UseCase12

--- src/UseCase12.java
+++ src/UseCase12.java
@@ -1,8 +1,8 @@
 import java.util.*;
 
-public class UseCase11 {
+public class UseCase12 {
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
-        System.out.println("Running UseCase11...");
+        System.out.println("Running UseCase12...");
     }
 }
